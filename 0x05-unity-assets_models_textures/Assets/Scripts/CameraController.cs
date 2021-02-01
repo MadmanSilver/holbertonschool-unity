f38dac0a0d7178ddd8e831b7dcf7cfb1beb1bbdb
Sean Taylor <silverwolf4350@gmail.com>
2021-02-01 01:46:50 -0600

    Created all files for mandatory tasks 0 to 14.

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+
+    public float sensitivity = 6.0f;
+    public float distance = 6.25f;
+
+    Vector3 cameraOffset;
+
+    float deltaX = 0f;
+    float deltaY = 0f;
+
+    void Start() {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaX += Input.GetAxisRaw("Mouse X");
+        deltaY = Mathf.Clamp(deltaY - Input.GetAxisRaw("Mouse Y"), -14f, 14f);
+    }
+
+    void LateUpdate() {
+        Quaternion rotation = Quaternion.Euler(deltaY * sensitivity, deltaX * sensitivity, 0);
+
+        transform.position = player.position + rotation * new Vector3(0, 0, -distance);
+        transform.LookAt(player);
+    }
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    public Collider player;
+    public Timer timer;
+    public Text timerText;
+
+    void OnTriggerEnter(Collider other) {
+        if (other == player) {
+            timer.Stop();
+            timerText.fontSize = 60;
+            timerText.color = Color.green;
+        }
+    }
+}
