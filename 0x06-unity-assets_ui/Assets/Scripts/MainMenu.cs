be7f808eaafc9e01614f9ea6baa5539949348454
Sean Taylor <silverwolf4350@gmail.com>
2021-02-15 23:02:58 -0600

    Created all files for tasks 0 to 6.

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LevelSelect(int level) {
+        SceneManager.LoadScene("Level" + level.ToString("00"));
+    }
+
+    public void Options() {
+        PlayerPrefs.SetInt("LastScene", SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene("Options");
+    }
+
+    public void Exit() {
+        Debug.Log("Exited");
+        Application.Quit();
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public void Back() {
+        int lastScene = PlayerPrefs.GetInt("LastScene");
+        PlayerPrefs.SetInt("LastScene", SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(lastScene);
+    }
+}
